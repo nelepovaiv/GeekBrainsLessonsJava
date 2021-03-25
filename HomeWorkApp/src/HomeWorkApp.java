fd652a0afeeaf2428e0511713862d89c2c6ee916
nelepovainna <2508Ytktgjdf>
2021-03-25 23:17:00 +0300

    Задание 1

--- HomeWorkApp/src/HomeWorkApp.java
+++ HomeWorkApp/src/HomeWorkApp.java
@@ -1,8 +1,48 @@
+import java.util.Random;
+
 public class HomeWorkApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    private static void compareNumbers() {
+        int a = 3;
+        int b = 8;
+
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    private static void printColor() {
+        int value = 10;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    private static void checkSumSign() {
+        int a = 5;
+        int b = -6;
+
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+
     }
 
     private static void printThreeWords() {
