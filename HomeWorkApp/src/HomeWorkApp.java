ff49d0f3566f1ed2c2356bddfb5711c2cdec1436
NelepovaInna <nelepovainna@192.168.0.133>
2021-03-25 23:02:49 +0300

    First lesson. Step 1

---
+++ HomeWorkApp/src/HomeWorkApp.java
@@ -0,0 +1,13 @@
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        printThreeWords();
+    }
+
+    private static void printThreeWords() {
+        System.out.println("_Orange");
+        System.out.println("_Banana");
+        System.out.println("_Apple");
+    }
+}
