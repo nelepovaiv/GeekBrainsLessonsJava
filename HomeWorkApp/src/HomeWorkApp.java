db43c76cc1ce6c8b0a450cd55ec88d854f84d81e
nelepovainna <2508Ytktgjdf>
2021-04-05 20:11:52 +0300

    ДЗ-3

--- HomeWorkApp/src/HomeWorkApp.java
+++ HomeWorkApp/src/HomeWorkApp.java
@@ -1,83 +1,121 @@
+import java.util.Arrays;
+
 public class HomeWorkApp {
 
     public static void main(String[] args) {
         /*
-          Задание 1
-          Написать метод, принимающий на вход два целых числа и проверяющий,
-          что их сумма лежит в пределах от 10 до 20 (включительно),
-          если да – вернуть true, в противном случае – false.
+          1. Задать целочисленный массив, состоящий из элементов 0 и 1.
+          Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
         */
-        boolean resultM1 = method1(3, 6);
-        System.out.println("Задание 1:" + resultM1);
+        array1Replacement(new int[]{0, 1, 0, 1, 0, 0, 1, 1, 0, 1});
 
         /*
            Задание 2
-           Написать метод, которому в качестве параметра передается целое число,
-           метод должен напечатать в консоль, положительное ли число передали или отрицательное.
-           Замечание: ноль считаем положительным числом.
+           Задать пустой целочисленный массив длиной 100.
+           С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
          */
-        String resultM2 = method2(-9);
-        System.out.println("Задание 2:" + resultM2);
+        array2MakeArray();
+
         /*
            Задание 3
-           Написать метод, которому в качестве параметра передается целое число.
-           Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
+           Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
          */
-        boolean resultM3 = method3(5);
-        System.out.println("Задание 3:" + resultM3);
+        array3Multiply(new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1});
+
         /*
            Задание 4
-           Написать метод, которому в качестве аргументов передается строка и число,
-           метод должен отпечатать в консоль указанную строку, указанное количество раз;
+           Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+           и с помощью цикла(-ов) заполнить его диагональные элементы единицами
+           (можно только одну из диагоналей, если обе сложно).
+           Определить элементы одной из диагоналей можно по следующему принципу:
+           индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
          */
-        method4("Hello", 4);
+        array4Diagonal(4);
+
         /*
            Задание 5
-           * Написать метод, который определяет, является ли год високосным,
-           и возвращает boolean (високосный - true, не високосный - false).
-           Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+           Написать метод, принимающий на вход два аргумента: len и initialValue,
+           и возвращающий одномерный массив типа int длиной len, каждая ячейка которого равна initialValue;
          */
-        method5(2020);
+        int[] array5 = array5WithLen(5, 3);
+        System.out.println("Задание 5. Массив:" + Arrays.toString(array5));
+
     }
 
-    private static void method5(int year) {
-        System.out.print("Задание 5:");
-        if (!(year % 4 == 0) || ((year % 100 == 0) && !(year % 400 == 0))) {
-            System.out.println(year + " г. не високосный");
-        } else {
-            System.out.println(year + " г. високосный");
-        }
+    private static int[] array5WithLen(int len, int initialValue) {
+        int[] retArray = new int[len];
+        System.out.println("Задание 5. Создать массив длинной " + len + " значениями " + initialValue);
+        Arrays.fill(retArray, initialValue);
+        return retArray;
     }
 
-    private static void method4(String str, int c) {
-        for (int i = 0; i < c; i++) {
-            System.out.println("Задание 4:" + str);
+    /*
+               Задание 4
+               Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+               и с помощью цикла(-ов) заполнить его диагональные элементы единицами
+               (можно только одну из диагоналей, если обе сложно).
+               Определить элементы одной из диагоналей можно по следующему принципу:
+               индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
+             */
+    private static void array4Diagonal(int size) {
+        int[][] array = new int[size][size];
+        for (int x = 0; x < array.length; x++) {
+            for (int y = 0; y < array.length; y++) {
+                if (x == y) {
+                    array[x][y] = 1;
+                }
+
+            }
         }
+
     }
 
-    private static boolean method3(int i) {
-        if (i >= 0) {
-            return false;
-        } else {
-            return true;
+    /*
+              Задание 3
+              Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+            */
+    private static void array3Multiply(int[] givenArray) {
+        System.out.println("Задание 3.Получен массив:" + Arrays.toString(givenArray));
+        for (int i = 0; i < givenArray.length; i++) {
+            int givenInt = givenArray[i];
+            if (givenInt < 6) {
+                givenArray[i] = givenInt * 2;
+            }
         }
+        System.out.println("Задание 3.Новый массив:" + Arrays.toString(givenArray));
     }
 
-    private static String method2(int i) {
-        if (i >= 0) {
-            return "положительное";
-        } else {
-            return "отрицательное";
+    /*
+   Задание 2
+   Задать пустой целочисленный массив длиной 100.
+   С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
+ */
+    private static void array2MakeArray() {
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
         }
+        System.out.println("Задание 2.Создан массив:" + Arrays.toString(array));
     }
 
-    private static boolean method1(int a, int b) {
-        int sum = a + b;
-        if (sum >= 10 && sum <= 20) {
-            return true;
-        } else {
-            return false;
+    /*
+    1. Задать целочисленный массив, состоящий из элементов 0 и 1.
+    Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+
+     */
+    private static void array1Replacement(int[] givenArray) {
+        System.out.println("Задание 1.Получен массив:" + Arrays.toString(givenArray));
+
+        for (int i = 0; i < givenArray.length; i++) {
+            int givenInt = givenArray[i];
+
+            if (givenInt == 0) {
+                givenArray[i] = 1;
+            } else if (givenInt == 1) {
+                givenArray[i] = 0;
+            }
         }
+        System.out.println("Задание 1.Инвертированный массив:" + Arrays.toString(givenArray));
     }
 
 
